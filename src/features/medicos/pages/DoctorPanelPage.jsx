06ccc659adbc4abcc904984c2a59f12b6e4b16ca
Alanis Forero <alanisforero@gmail.com>
2025-04-14 18:16:01 -0500

    foto del medico 6 horas

--- src/features/medicos/pages/DoctorPanelPage.jsx
+++ src/features/medicos/pages/DoctorPanelPage.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const DoctorPanelPage = () => {
     const navigate = useNavigate();
+    const [doctorImage, setDoctorImage] = useState('');
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                const email = user.email;
+                try {
+                    const res = await axios.get(
+                        `http://localhost:8080/api/medicos/buscar-por-correo?correo=${encodeURIComponent(email)}`
+                    );
+                    console.log("📦 Respuesta completa:", res.data);
+                    const doctorData = res.data;
+                    setDoctorImage(doctorData.urlImagen);
+                } catch (error) {
+                    console.error('❌ Error al obtener los detalles del médico:', error);
+                }
+            } else {
+                console.error('❌ Usuario no autenticado');
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+
+
+
 
     return (
         <div style={{ padding: '2rem', textAlign: 'center' }}>
             <h1>Bienvenido al panel del médico</h1>
+            {doctorImage && <img src={doctorImage} alt="Doctor" style={{ width: '150px', borderRadius: '50%' }} />}
             <button
                 style={{ marginTop: '1rem', padding: '0.5rem 1rem', fontSize: '1rem' }}
                 onClick={() => navigate('/register-patient')}
